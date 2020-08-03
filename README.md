6f31662b035c763634f63181734c8189d1c83937
Andrew Gorokhovets <allmywebwork@gmail.com>
2020-08-03 15:48:20 +0300

    A basic module implementation.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# video_provider_custom_url
+# Video provider custom url
+This Drupal module allow using custom third party url as a source of video content.
+
+This module use FontAwesome icon but not required it.
---
+++ src/Plugin/video/Provider/CustomUrlVideo.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\video_provider_custom_url\Plugin\video\Provider;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+use Drupal\video\ProviderPluginBase;
+
+/**
+ * A custom url provider plugin.
+ *
+ * Regexp url validator was taking from here
+ * https://daringfireball.net/2010/07/improved_regex_for_matching_urls.
+ *
+ * @VideoEmbeddableProvider(
+ *   id = "customurl",
+ *   label = @Translation("Сustom video url"),
+ *   description = @Translation("Custom url video provider"),
+ *   regular_expressions = {
+ *     "/\b(?:(?:https?):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i"
+ *   },
+ *   mimetype = "video/customurl",
+ *   stream_wrapper = "customurl"
+ * )
+ */
+class CustomUrlVideo extends ProviderPluginBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * Provides helpers to operate on files and stream wrappers.
+   *
+   * @var \Drupal\file\FileStorage
+   */
+  protected $fileStorage;
+
+  /**
+   * Provides helpers to cache remoteThumbnailUri string.
+   *
+   * @var string
+   */
+  protected $remoteThumbnailUri;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function renderEmbedCode($settings) {
+    $image_thumbnail_uri = $this->getRemoteThumbnailUri();
+    $anchor = $this->t('Watch the video');
+    if (!empty($image_thumbnail_uri)) {
+      $anchor = [
+        '#theme' => 'image_style',
+        '#style_name' => 'video_thumbnail',
+        '#uri' => $image_thumbnail_uri,
+      ];
+    }
+
+    $output['container'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => 'video-provider-custom-url-container',
+      ],
+    ];
+
+    $output['container']['link'] = [
+      '#type' => 'link',
+      '#attributes' => ['target' => '_blank', 'class' => 'video-link'],
+      '#title' => $anchor,
+      '#url' => Url::fromUri($this->getVideoMetadata()['uri']),
+    ];
+
+    $output['#attached']['library'][] = 'video_provider_custom_url/video_provider_custom_url.video_thumbnail';
+
+    return $output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRemoteThumbnailUrl() {
+    return file_create_url($this->getRemoteThumbnailUri());
+  }
+
+  /**
+   * Helper function to get file storage service.
+   *
+   * @return \Drupal\Core\Entity\EntityStorageInterface|\Drupal\file\FileStorage
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  protected function getFileStorage() {
+    if (!empty($this->fileStorage)) {
+      return $this->fileStorage;
+    }
+
+    return \Drupal::entityTypeManager()->getStorage('file');
+  }
+
+  /**
+   * Helper function to return image thumbnail URI.
+   */
+  protected function getRemoteThumbnailUri() {
+    if (!empty($this->remoteThumbnailUri)) {
+      return $this->remoteThumbnailUri;
+    }
+
+    $data = $this->getVideoMetadata();
+    if (@empty($data['thumbnail']['target_id'])) {
+      return NULL;
+    }
+    $data = $this->getVideoMetadata();
+    $file = $this->getFileStorage()->load($data['thumbnail']['target_id']);
+
+    return $file->getFileUri();
+  }
+}
